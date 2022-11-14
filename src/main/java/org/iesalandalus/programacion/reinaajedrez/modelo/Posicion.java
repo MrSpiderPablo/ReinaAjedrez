30ff84d567312ad41e78a442c79d5f4e6377fdd1
pablo <pabloramosblanquez@gmail.com>
2022-11-14 12:29:03 +0100

    Añado los get y los set a Posicion y les implemento la excepcion a cada uno

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -4,4 +4,31 @@ public class Posicion
 {
 	private int fila;
 	private char columna;
+	
+	
+	public int getFila() 
+	{
+		return fila;
+	}
+	public void setFila(int fila) 
+	{
+		if (fila < 1 || fila > 8) {
+			throw new IllegalArgumentException("Esta posicion no es correcta");
+		}
+		this.fila = fila;
+	}
+	public char getColumna() 
+	{
+		return columna;
+	}
+	public void setColumna(char columna) 
+	{
+		if (columna < 'a' || columna > 'h') 
+		{
+			throw new IllegalArgumentException("Esta posicion no es correcta");
+		}
+		this.columna = columna;
+	}
+	
+	
 }
