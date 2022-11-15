6c8bee1695240ed3b16a194ca57d048a3ddb4b7a
pablo <pabloramosblanquez@gmail.com>
2022-11-15 13:16:45 +0100

    Añado los get y set de color y posicion

---
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -0,0 +1,41 @@
+package org.iesalandalus.programacion.reinaajedrez.modelo;
+
+public class Reina 
+{
+	private Color color;
+	private Posicion posicion;
+	
+	
+	public Color getColor() 
+	{
+		return color;
+	}
+	
+	
+	public void setColor(Color color) 
+	{
+		if (color == null) 
+		{
+			throw new NullPointerException("El color no puede ser nulo");
+		}
+		this.color = color;
+	}
+	
+	
+	public Posicion getPosicion() 
+	{
+		return posicion;
+	}
+	
+	
+	public void setPosicion(Posicion posicion) 
+	{
+		if (posicion == null) 
+		{
+			throw new NullPointerException("No se aceptan posiciones nulas");
+		}
+		this.posicion = posicion;
+	}
+	
+	
+}
